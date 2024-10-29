7b32ea4d1dd9cb16742c23c30bd2dee3b987e077
Stanislav <stanislav.titarenko@gmail.com>
2024-10-29 17:24:46 +0300

    Before migration

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'users',
+    'habits',
 
 ]
 
@@ -114,6 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
